92cca65f489a0bf8855cdd07a897d738744a7127
Krzysztof Ropiak <kaszpiro@gmail.com>
2022-04-10 12:03:42 +0200

    aktualizacja skryptów z zajęć

--- __kod_z_zajęć/Zadania_lab_4.py
+++ __kod_z_zajęć/Zadania_lab_4.py
@@ -42,6 +42,102 @@ def zadanie_3():
         print('Warunki nie są spełnione')
 
 
+def zadanie_4():
+    # case 1
+    for x in range(0, 50):
+        if x % 5 == 0:
+            print(x)
+
+    # case 2
+    for x in range(0, 50, 5):
+        print(x)
+
+
+def zadanie_5():
+    while True:
+        liczby = input('Podaj liczby oddzielone spacją\n')
+        # warunek stopu funkcji
+        if len(liczby) == 0:
+            break
+
+        liczby = liczby.split(' ')
+
+        for index, elem in enumerate(liczby):
+            liczby[index] = int(elem)
+
+        # wyświetlamy kwadraty tych liczb
+        for liczba in liczby:
+            print(f'kwadrat liczby {liczba} to {liczba**2}')
+
+def zadanie_6():
+    lista = []
+    while True:
+        wejscie = input('Podaj liczbę\n')
+        # warunek stopu
+
+        if wejscie == 'stop':
+            break
+        if wejscie.isnumeric():
+            lista.append(int(wejscie))
+
+    return lista
+
+
+def zadanie_7():
+    wejscie = input('Podaj liczbę\n')
+    suma = 0
+
+    for znak in wejscie:
+        if znak.isdigit():
+            suma = suma + int(znak)
+
+    print(f'Suma cyfr z {wejscie} to {suma}')
+
+
+def zadanie_8():
+    poziom_wiezy = int(input())
+    # pętla dla linii
+    for linia in range(1, poziom_wiezy+1):
+        # pętla dla znaków w linii
+        for litera in range(linia):
+            print('A', end='')
+        print()
+
+
+def zadanie_8_bardziej_optymalnie():
+    poziom_wiezy = int(input('Podaj wysokość drzewka\n'))
+    znak = input('Podaj znak dla drzewka\n')
+    # pętla dla linii
+    for linia in range(1, poziom_wiezy+1):
+        print(znak * linia)
+
+
+# type hints - podpowiadanie typu, sugerowanie typu
+def zadanie_8_bardziej_optymalnie_2(wysokosc: int, znak: str) -> None:
+    # poziom_wiezy = int(input('Podaj wysokość drzewka\n'))
+    # znak = input('Podaj znak dla drzewka\n')
+    # pętla dla linii
+    for linia in range(1, wysokosc+1):
+        print(znak * linia)
+
+
+# dodanie parametru tabliczki
+def zadanie_9(n=10):
+    # krok 1 - wypisanie etykiet kolumn
+    for etykieta in range(0, n+1):
+        if etykieta == 0:
+            print("    ", end=' ')
+        else:
+            print(f'{etykieta:>4}', end=' ')
+    print()
+    # krok 2 - wypisujemy jedną etykietę wiersza i wszystkie wartości w danym wierszu
+    for wiersz in range(1, n+1):
+        print(f'{wiersz:>4}', end=' ')
+        for kolumna in range(1, n+1):
+            print(f'{wiersz * kolumna:>4}', end=' ')
+        print()
+
+
 if __name__ == '__main__':
 
     # ilosc_spacji = zadanie_1_alternatywnie()
@@ -49,4 +145,20 @@ if __name__ == '__main__':
 
     # zadanie_1()
     # zadanie_2()
-    zadanie_3()
+    # zadanie_3()
+    # zadanie_5()
+    # print(zadanie_6())
+    # zadanie_7()
+    # zadanie_8()
+    # zadanie_8_bardziej_optymalnie_2(5, '#')
+    # zadanie_8_bardziej_optymalnie_2(3, 'E')
+    # zadanie_8_bardziej_optymalnie_2(znak='.', wysokosc=5)
+    # zadanie_8_bardziej_optymalnie_2(4)
+    # zadanie_8_bardziej_optymalnie_2(znak='#')
+    zadanie_9()
+    zadanie_9(12)
+    zadanie_9(100)
+
+
+
+
